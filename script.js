ab33868eeaf316b80377dbae14b3ba33a622481a
Tailung1 <ramazsamitashvili11@gmail.com>
2025-01-30 21:13:57 +0400

    cancel change

--- script.js
+++ script.js
@@ -61,7 +61,6 @@ function endQuiz() {
     questionsContainer.innerHTML=`<h2>Quiz Completed </h2>`
     optionsContainer.textContent="";
     submitBtn.style.display='none'
-    
     scoreElement.innerHTML=`Total Score: ${score}`
     scoreElement.style.cssText="color:blue;font-size:30px"
 }
